f7c9490a0a24c9ca44f530506a5761dd15ca48aa
Rios <60994197+endorphins-jpg@users.noreply.github.com>
2020-11-23 19:34:14 -0300

    Pares e Ímpares

---
+++ 1259.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int pontPAR(void const *a, void const *b){
+    return (*(int*)a - *(int*)b);
+}
+int pontIMP(void const *a, void const *b){
+    return (*(int*)b - *(int*)a);
+}
+
+int main(){
+    int n, i, num, par, impar;
+    scanf("%d", &n);
+    int numpar[n];
+    int numimp[n];
+
+    par = 0;
+    impar = 0;
+    for(i = 0; i < n; i++){
+        scanf("%d", &num);
+        if(num%2 == 0){
+            numpar[par] = num;
+            par++;
+        }
+        else{
+            numimp[impar] = num;
+            impar++;
+        }
+    }
+
+    printf("\n");
+    qsort(numpar, par, sizeof(int), pontPAR);
+    qsort(numimp, impar, sizeof(int), pontIMP);
+
+    for(i = 0; i < par; i++){
+        printf("%d\n",numpar[i]);
+    }
+    for(i = 0; i < impar; i++){
+        printf("%d\n",numimp[i]);
+    }
+
+    return 0;
+}
